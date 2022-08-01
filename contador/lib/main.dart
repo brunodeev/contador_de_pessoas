493b3aeda19cd76e010259d313937f64f2cb8ff1
Bruno César <bcgmeireles@gmail.com>
2022-08-01 13:31:24 -0300

    Ajustes finais no funcionamento e estilo

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -43,6 +43,9 @@ class _HomePageState extends State<HomePage> {
     print(count);
   }
 
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,41 +60,48 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text("Pode entrar!",
+            Text(isFull ? "Lotado!" : "Pode entrar!",
                 style: TextStyle(
                     fontSize: 40,
-                    color: Colors.white,
+                    color: isFull
+                        ? Color.fromARGB(255, 255, 87, 87)
+                        : Colors.white,
                     fontWeight: FontWeight.w600)),
             Padding(
               padding: EdgeInsets.all(45),
               child: Text("$count",
                   style: TextStyle(
                     fontSize: 100,
-                    color: Colors.white,
+                    color: isFull
+                        ? Color.fromARGB(255, 255, 87, 87)
+                        : Colors.white,
                   )),
             ),
             Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               TextButton(
-                onPressed: decrement,
+                onPressed: isEmpty ? null : decrement,
                 style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
+                    backgroundColor: isEmpty
+                        ? Color.fromARGB(164, 255, 85, 85)
+                        : Color.fromARGB(255, 255, 87, 87),
                     fixedSize: const Size(100, 100),
                     primary: Colors.black,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(24))),
                 child: const Text("Saiu",
                     style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
+                      color: Colors.white,
+                      fontSize: 20,
                     )),
               ),
               const SizedBox(
                 width: 40,
               ),
               TextButton(
-                onPressed: increment,
+                onPressed: isFull ? null : increment,
                 style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
+                    backgroundColor:
+                        isFull ? Colors.white.withOpacity(0.3) : Colors.white,
                     fixedSize: const Size(100, 100),
                     primary: Colors.black,
                     shape: RoundedRectangleBorder(
